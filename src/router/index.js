f30d89e4ff2ed8b7017ef40078709ace9005e523
vlad <vlad.it.world@mail.ru>
2023-05-30 00:08:18 +0500

    Выгрузка

---
+++ src/router/index.js
@@ -0,0 +1,72 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      components: {
+        default: () => import("../views/Home.vue"),
+        sidebar: () => import("../components/SideNav.vue"),
+      },
+    },
+    {
+      path: "/dashboard",
+      components: {
+        default: () => import("../views/Dashboard.vue"),
+        sidebar: () => import("../components/SideNav.vue"),
+      },
+    },
+    {
+      path: "/personality-types",
+      components: {
+        default: () => import("../views/PersonalityTypes.vue"),
+        sidebar: () => import("../components/SideNav.vue"),
+      },
+    },
+    {
+      path: "/courses",
+      components: {
+        default: () => import("../views/Courses.vue"),
+        sidebar: () => import("../components/SideNav.vue"),
+      },
+    },
+    {
+      path: "/professions",
+      components: {
+        default: () => import("../views/Professions.vue"),
+        sidebar: () => import("../components/SideNav.vue"),
+      },
+    },
+    {
+      path: "/personality-tests",
+      components: {
+        default: () => import("../views/PersonalityTests.vue"),
+        sidebar: () => import("../components/SideNav.vue"),
+      },
+    },
+    {
+      path: "/courses-tests",
+      components: {
+        default: () => import("../views/CoursesTests.vue"),
+        sidebar: () => import("../components/SideNav.vue"),
+      },
+    },
+    {
+      path: "/test-courses-constructor",
+      components: {
+        default: () => import("../views/ConstructTestCourses.vue"),
+        sidebar: () => import("../components/SideNav.vue"),
+      },
+    },
+    {
+      path: "/person-type-constructor",
+      components: {
+        default: () => import("../views/ConstructNewPersonType.vue"),
+        sidebar: () => import("../components/SideNav.vue"),
+      },
+    },
+  ],
+});
+
+export default router;
